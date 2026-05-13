1d508e4c874051cc1ad76c076e9ec9150cae1948
amanieleth-hub <amanieleth@gmail.com>
2026-05-13 20:06:07 +0300

    Update farmers.js console

--- assets/js/farmers.js
+++ assets/js/farmers.js
@@ -5,10 +5,15 @@ import {
   getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+console.log("farmers.js loaded");
+
 const firebaseConfig = {
   apiKey: "AIzaSyAHKnemdl-A2p_dHK43LpwTAxmcAbyFZGk",
   authDomain: "ellidun-microlot-coffee.firebaseapp.com",
   projectId: "ellidun-microlot-coffee",
+  storageBucket: "ellidun-microlot-coffee.firebasestorage.app",
+  messagingSenderId: "286011718104",
+  appId: "1:286011718104:web:bb4f4e018d28ffdb2c1aad"
 };
 
 const app = initializeApp(firebaseConfig);
