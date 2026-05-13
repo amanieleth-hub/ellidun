c651e9f41db4adc300aaa4828e795b7ac042f3bc
amanieleth-hub <amanieleth@gmail.com>
2026-05-13 20:05:15 +0300

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -14,6 +14,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyAHKnemdl-A2p_dHK43LpwTAxmcAbyFZGk",
   authDomain: "ellidun-microlot-coffee.firebaseapp.com",
   projectId: "ellidun-microlot-coffee",
+  storageBucket: "ellidun-microlot-coffee.firebasestorage.app",
+  messagingSenderId: "286011718104",
+  appId: "1:286011718104:web:bb4f4e018d28ffdb2c1aad"
 };
 
 const app = initializeApp(firebaseConfig);
