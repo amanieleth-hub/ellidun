8b37eecface5e2deb22995a8324fed0b7107f8ef
amanieleth-hub <amanieleth@gmail.com>
2026-05-13 21:58:14 +0300

    Update lotadmin.js

--- assets/js/lotadmin.js
+++ assets/js/lotadmin.js
@@ -4,16 +4,6 @@ import {
   doc, setDoc, addDoc, updateDoc, query, where
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// ── Firebase config ───────────────────────────────────────────────────────
-const firebaseConfig = {
-  apiKey: "AIzaSyAHKnemdl-A2p_dHK43LpwTAxmcAbyFZGk",
-  authDomain: "ellidun-microlot-coffee.firebaseapp.com",
-  projectId: "ellidun-microlot-coffee",
-  storageBucket: "ellidun-microlot-coffee.firebasestorage.app",
-  messagingSenderId: "286011718104",
-  appId: "1:286011718104:web:bb4f4e018d28ffdb2c1aad"
-};
-
 // Upload images
 const CLOUDINARY_CLOUD_NAME = "devkhvfvq";   // from Cloudinary dashboard
 const CLOUDINARY_UPLOAD_PRESET = "farmersimages"; // the unsigned preset you created
